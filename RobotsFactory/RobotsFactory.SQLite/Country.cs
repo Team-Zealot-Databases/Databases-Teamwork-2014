1fa924be2b2087d9e52fbf5ed1878cd78dc0dda7
Martin Nikolov <flextry@gmail.com>
2014-09-03 19:35:52 +0300

    SQLite project added

---
+++ RobotsFactory/RobotsFactory.SQLite/Country.cs
@@ -0,0 +1,16 @@
+﻿namespace RobotsFactory.SQLite
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+
+    public class Country
+    {
+        [Key]
+        public long CountryId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal TaxRate { get; set; }
+    }
+}
---
+++ RobotsFactory/RobotsFactory.SQLite/SQLiteDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace RobotsFactory.SQLite
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class SQLiteDbContext : DbContext
+    {
+        public SQLiteDbContext()
+            : base("RobotsFactorySqlite")
+        {
+            //this.Database.Connection.ConnectionString = @"Data Source=..\..\CountryTaxesDb.sqlite";
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
