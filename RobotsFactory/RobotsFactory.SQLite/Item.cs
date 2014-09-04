8b08c2460a85d8f3581dfbdfeb776076e8c416a8
Martin Nikolov <flextry@gmail.com>
2014-09-04 19:16:34 +0300

    Upload last version of the project. Project done!

---
+++ RobotsFactory/RobotsFactory.SQLite/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace RobotsFactory.SQLite
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+
+    public class Item
+    {
+        [Key]
+        public int ItemId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Expense { get; set; }
+    }
+}
